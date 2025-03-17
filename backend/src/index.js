52bd8b5842c1715375ebac8268f854e40b2b5b57
lalit452 <lalitkumarmeena452@gmail.com>
2025-03-17 16:23:13 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,35 @@
+// const express = require("express")
+import express from "express"
+import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import messageRoutes from "./routes/message.route.js"
+import cors from "cors";
+
+import {connectDB} from './lib/db.js'
+import { app, server } from "./lib/socket.js"
+dotenv.config()
+// const app = express();    // delete because soket.io me app create kar diya hai
+// import { app, server } from "./lib/socket.js"  <------ we imported this app from soket.io this will we will use
+
+const PORT = process.env.PORT || 5001;
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+// this will allow you to extract json data out of body
+app.use(express.json()); 
+app.use(cookieParser());
+app.use(cors({ 
+    // origin: "http://localhost:5173",
+    origin: FRONTEND_URL,
+    credentials: true
+}))
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// app.listen(PORT, ()=>{             <------------- app ko server se replace kar de to use socket.io
+server.listen(PORT, ()=>{
+    console.log("server is running on PORT :" + PORT);
+    console.log(`CORS allowed for: ${FRONTEND_URL}`);
+    connectDB()
+}) 
